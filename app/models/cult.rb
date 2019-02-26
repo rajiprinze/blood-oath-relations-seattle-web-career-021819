2370a01952e8a777f346b7806f87c137850a3908
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-26 21:01:22 +0000

    Automatically backed up by Learn

---
+++ app/models/cult.rb
@@ -0,0 +1,13 @@
+class Cult
+
+attr_accessor :name, :location, :year, :slogan
+
+def initialize(name, location, year, slogan)
+  @name = name
+  @location = location
+  @year = year
+  @slogan = slogan
+end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,7 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-
+cult1 = Cult.new("Human", "Seattle", "2019", "Human's are weird.")
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
